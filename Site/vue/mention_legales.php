7d82a3f159b094c08ebb1b39ecc3a13d76bbf8d5
cmignon96 <chris.mignon@gmail.com>
2017-06-08 11:44:33 +0200

    Add files via upload

---
+++ Site/vue/mention_legales.php
@@ -0,0 +1,51 @@
+<body id="bodyautre">
+  <div id="banniereautre" class="banner2">
+    <div class="diamond">
+      <div></div>
+    </div>
+    <h1 class="text"> Mentions légales </h1>
+  </div>
+
+<div id="contenu" style="margin-left: 2%; margin-right:2%;">
+  <form class='basic-grey conditiono' style="text-align:justify">
+  Merci de lire avec attention les différentes modalités d’utilisation du présent site avant d’y parcourir ses pages. En vous connectant sur ce site, vous acceptez sans réserves les présentes modalités. Aussi, conformément à l’article n°6 de la Loi n°2004-575 du 21 Juin 2004 pour la confiance dans l’économie numérique, les responsables du présent site internet <a href="http://cosyhouse.fr">cosyhouse.fr</a> sont le groupe G10E de l'ISEP.
+  </br></br></br>
+  <h3>Editeur du Site</h3>
+  GROUPE APP G10E
+  10 rue de vanves, Issy-les-Moulineaux 92130
+  Téléphone : 01 23 45 67 89
+  Email : cosyhouse6@gmail.com
+  Site Web : <a href="http://cosyhouse.fr">http://cosyhouse.fr</a>
+  </br></br></br>
+  <h3>Développement</h3>
+  G10E ISEP
+  Adresse : 10 rue de vanves issy les moulineaux
+  Site Web : <a href="http://cosyhouse.fr">http://cosyhouse.fr</a>
+  </br></br></br>
+  <h3>Conditions d’utilisation</h3>
+  Ce site (<a href="http://cosyhouse.fr">http://cosyhouse.fr</a>) est proposé en différents langages web (HTML5, Javascript, CSS, etc…) pour un meilleur confort d'utilisation et un graphisme plus agréable, nous vous recommandons de recourir à des navigateurs modernes comme Internet explorer, Safari, Firefox, Google Chrome, etc…
+  Les mentions légales ont été générées sur le site <a title="générateur de mentions légales pour site internet gratuit" href="http://www.generateur-de-mentions-legales.com">Générateur de mentions légales</a>, offert par <a title="imprimerie paris, imprimeur paris" href="http://welye.com">Welye</a>.
+  <span>GROUPE APP G10E<b> </b></span>met en œuvre tous les moyens dont elle dispose, pour assurer une information fiable et une mise à jour fiable de ses sites internet. Toutefois, des erreurs ou omissions peuvent survenir. L'internaute devra donc s'assurer de l'exactitude des informations auprès de , et signaler toutes modifications du site qu'il jugerait utile. n'est en aucun cas responsable de l'utilisation faite de ces informations, et de tout préjudice direct ou indirect pouvant en découler.
+  </br></br></br>
+  <h3>Liens hypertextes</h3> Les sites internet de peuvent offrir des liens vers d’autres sites internet ou d’autres ressources disponibles sur Internet. GROUPE APP G10E ne dispose d'aucun moyen pour contrôler les sites en connexion avec ses sites internet. Elle ne peut être tenue pour responsable de tout dommage, de quelque nature que ce soit, résultant du contenu de ces sites ou sources externes, et notamment des informations, produits ou services qu’ils proposent, ou de tout usage qui peut être fait de ces éléments. Les risques liés à cette utilisation incombent pleinement à l'internaute, qui doit se conformer à leurs conditions d'utilisation.
+  Les utilisateurs, les abonnés et les visiteurs des sites internet de ne peuvent mettre en place un hyperlien en direction de ce site sans l'autorisation expresse et préalable de GROUPE APP G10E.
+  Dans l'hypothèse où un utilisateur ou visiteur souhaiterait mettre en place un hyperlien en direction d’un des sites internet de GROUPE APP G10E, il lui appartiendra d'adresser un email accessible sur le site afin de formuler sa demande de mise en place d'un hyperlien. GROUPE APP G10E se réserve le droit d’accepter ou de refuser un hyperlien sans avoir à en justifier sa décision.
+  </br></br></br>
+  <h3>Limitation contractuelles sur les données</h3>
+  Les informations contenues sur ce site sont aussi précises que possible et le site remis à jour à différentes périodes de l’année, mais peut toutefois contenir des inexactitudes ou des omissions. Si vous constatez une lacune, erreur ou ce qui parait être un dysfonctionnement, merci de bien vouloir le signaler par email, à l’adresse cosyhouse6@gmail.com, en décrivant le problème de la manière la plus précise possible (page posant problème, type d’ordinateur et de navigateur utilisé, …).
+  Tout contenu téléchargé se fait aux risques et périls de l'utilisateur et sous sa seule responsabilité. En conséquence, ne saurait être tenu responsable d'un quelconque dommage subi par l'ordinateur de l'utilisateur ou d'une quelconque perte de données consécutives au téléchargement. <span style="color: #323333;">De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour</span>
+  Les liens hypertextes mis en place dans le cadre du présent site internet en direction d'autres ressources présentes sur le réseau Internet ne sauraient engager la responsabilité de GROUPE APP G10E.
+  </br></br></br>
+  <h3>Propriété intellectuelle</h3>
+  Tout le contenu du présent sur le site <a href="http://cosyhouse.fr">http://cosyhouse.fr</a>, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de la société à l'exception des marques, logos ou contenus appartenant à d'autres sociétés partenaires ou auteurs.
+  Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l'accord exprès par écrit de GROUPE APP G10E. Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur.
+  </br></br></br>
+  <h3>Données personnelles</h3>
+  De manière générale, vous n’êtes pas tenu de nous communiquer vos données personnelles lorsque vous visitez notre site Internet <a href="http://cosyhouse.fr">http://cosyhouse.fr</a>.
+  Cependant, ce principe comporte certaines exceptions. En effet, pour certains services proposés par notre site, vous pouvez être amenés à nous communiquer certaines données telles que : votre nom, votre fonction, le nom de votre société, votre adresse électronique, et votre numéro de téléphone. Tel est le cas lorsque vous remplissez le formulaire qui vous est proposé en ligne, dans la rubrique « contact ». Dans tous les cas, vous pouvez refuser de fournir vos données personnelles. Dans ce cas, vous ne pourrez pas utiliser les services du site, notamment celui de solliciter des renseignements sur notre société, ou de recevoir les lettres d’information.
+  Enfin, nous pouvons collecter de manière automatique certaines informations vous concernant lors d’une simple navigation sur notre site Internet, notamment : des informations concernant l’utilisation de notre site, comme les zones que vous visitez et les services auxquels vous accédez, votre adresse IP, le type de votre navigateur, vos temps d'accès. De telles informations sont utilisées exclusivement à des fins de statistiques internes, de manière à améliorer la qualité des services qui vous sont proposés. Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.
+</div>
+</form>
+</body>
+
+</html>
