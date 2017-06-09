d073417c4a0ac78a3e2208271d8a198007090a32
yassminebo <yassmine.bouali@isep.fr>
2017-06-09 12:03:30 +0200

    Create inscription.php

--- Site/vue/inscription.php
+++ Site/vue/inscription.php
@@ -10,8 +10,7 @@
   <h3>Clé d'inscription</h3>
   <div id="eveC">
     <input type="text" name="cle" placeholder=" Numéro de série"  required title="Vous trouverez votre clé d'inscription dans votre produit DOMISEP"/>
-    <span style="display:none" id="cl">Disponible </span>
-    <span style="display:none" id="clN">Indisponible </span>
+
   </div>
 
 
@@ -21,16 +20,15 @@
       <input id="nomm" type="text" name="nom" placeholder=" Nom"  required  title="Le nom doit contenir entre 2 et 25 caractères"/>
       <input id="prenomm" type="text" name="prenom" placeholder=" Prénom" required title="Le prénom doit contenir entre 2 et 25 caractères"/>
       <div id="eveM">
-      <input id="mailm" type="email" name="email" placeholder="&#x1F4E7 E-mail" required title="E-mail du type : user@domain.com"/>
-      <span style="display:none" id="mai">Disponible </span>
-      <span style="display:none" id="maiN">Indisponible </span>
+      <input id="mailm" type="email" name="email" placeholder=" E-mail" required title="E-mail du type : user@domain.com"/>
+     
       </div>
 
      </p>
 
 
    <p>
-   <input id="telm" type="text" name="tel" onkeydown="if(event.keyCode==32) return false;" placeholder="&#128222 Téléphone" required title="Numéro de téléphone sans espace du type : 0102030405"/>
+   <input id="telm" type="text" name="tel" onkeydown="if(event.keyCode==32) return false;" placeholder=" Téléphone" required title="Numéro de téléphone sans espace du type : 0102030405"/>
 
    </p>
    <p> Date de naissance</p>
@@ -52,8 +50,7 @@
        <p>
            <div id="eveL" >
          <input id="loginm" type="text" name="login" placeholder="Pseudo utilisé pour la connexion" size="25" required title="Le login doit contenir entre 3 et 25 caractères"/>
-          <span style="display:none" id="log">Disponible &#x2713</span>
-          <span style="display:none" id="logN">Indisponible &#x2715</span>
+         
 
             </div>
        </p>
@@ -80,14 +77,6 @@
 
 
 </form>
-<link rel="stylesheet" href="vue/jquerystyle/jquery-ui.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script src="vue/datepicker-fr.js"></script>
-
-<script src="vue/securite_formulaire.js" type="text/javascript"></script>
 
 
 </body>
