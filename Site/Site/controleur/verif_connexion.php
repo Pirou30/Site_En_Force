a6b5aa63b762b11193fe7e7fea7f9bb3cd1c8cf0
Pirouuu <Piruntha.nadarajah@isep.fr>
2017-06-12 22:07:49 +0200

    Ajout de condition "If"

--- Site/Site/controleur/verif_connexion.php
+++ Site/Site/controleur/verif_connexion.php
@@ -1,3 +1,4 @@
+
 <?php
 
 // Ceci permet de gérer le formulaire de connexion des utilisateurs
@@ -5,5 +6,14 @@ require("modele/gestion_utilisateur.php");
 
 $reponse = mdp($db,$_POST['login']); // appel de la fonction qui se trouve dans gestion utilisateur
 
-
+if($reponse->rowcount()==0)      //Si l'utilisateur non trouvé dans la base de données
+{
+  $_SESSION['erreur'] = "l'utilisateur est inconnu</h3>";
+  header("Location: index.php?page=connexion"); // Redirection vers la page d'erreur
+  exit(); //Arret du Script
+}
+else   // Dans le cas contraire
+{
+  
+  
 ?>
