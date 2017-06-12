482e48440536547e933b667377c37dd1a2f5f7f2
Pirouuu <Piruntha.nadarajah@isep.fr>
2017-06-12 16:52:59 +0200

    Create verif_connexion.php

--- Site/Site/controleur/verif_connexion.php
+++ Site/Site/controleur/verif_connexion.php
@@ -3,4 +3,7 @@
 // Ceci permet de gérer le formulaire de connexion des utilisateurs
 require("modele/gestion_utilisateur.php");
 
+$reponse = mdp($db,$_POST['login']); // appel de la fonction qui se trouve dans gestion utilisateur
+
+
 ?>
