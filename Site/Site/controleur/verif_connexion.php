155905fdbe8fe988a80891d5ee94177dd7b15a29
Pirouuu <Piruntha.nadarajah@isep.fr>
2017-06-13 14:09:19 +0200

    Ajout de condition consernant le mdp
    comparaison de mdp entré et stocké 
    Hachage avec la fonction sha1()

--- Site/Site/controleur/verif_connexion.php
+++ Site/Site/controleur/verif_connexion.php
@@ -9,11 +9,22 @@ $reponse = mdp($db,$_POST['login']); // appel de la fonction qui se trouve dans 
 if($reponse->rowcount()==0)      //Si l'utilisateur non trouvé dans la base de données
 {
   $_SESSION['erreur'] = "l'utilisateur est inconnu</h3>";
-  header("Location: index.php?page=connexion"); // Redirection vers la page d'erreur
+  header("Location: index.php?page=connexion"); // Redirection vers la page de connexion
   exit(); //Arret du Script
 }
+
 else   // Dans le cas contraire
 {
+  $ligne = $reponse->fetch(); //fetch va chercher dans $reponse et organiser les champs dans $ligne
   
+  if(sha1($_POST['mdp'])!=$ligne['mot_de_passe']) //Comparaison du mpd entré avec celui stocké dans la base de donnée (hachage)
+    {                                             //Lorsque ces 2 mdp sont différents
+    $_SESSION['erreur'] = "le mot de passe est incorrect</h3>";
+    header("Location: index.php?page=connexion");
+    exit();
+  }
   
+  else      // Dans le cas où le mdp entré correspond au mdp stocké dans la db
+  {
+    
 ?>
