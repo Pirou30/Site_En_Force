477750c7f2825ef650cb578d5d8b64271722d686
chrismignon1996 <chris.mignon@icloud.com>
2017-06-14 15:33:11 +0200

    Create gestion_capteur.php

--- Site/Site/modele/gestion_capteur.php
+++ Site/Site/modele/gestion_capteur.php
@@ -24,3 +24,29 @@ function ajout_objet($db)
   $cle = $_POST['cle'];
   $req2 = $db -> query("UPDATE numero_serie SET activation = 0 WHERE numero_de_serie = '".$cle."'");
  }
+
+// fonction qui cherche tous les capteurs dans la base de données dépendant de quelle piece ou ils se trouvent
+  function recup_all_objet($db)
+  {
+    $id_utilisateur = $_SESSION['id_utilisateur']; // variable qui déclare l'ouverture d'une session avec un utilisateur
+    $reponse = $db->query("SELECT * FROM objet_connecte WHERE id_piece IN (SELECT id_piece FROM posseder WHERE id_utilisateur = '".$id_utilisateur."')");
+    return $reponse;
+  }
+
+
+
+// fonction qui cherche tous les capteurs dans la base de données
+  function recup_all_serie($db)
+  {
+    $reponse = $db->query('SELECT * FROM numero_serie');
+    return $reponse;
+  }
+
+
+
+// fonction qui cherche toutes les catégories dans la base de données
+  function recup_all_categorie($db)
+  {
+    $reponse = $db->query('SELECT * FROM categorie');
+    return $reponse;
+  }
