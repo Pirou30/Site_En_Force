7ad438df8ef9d12151da0d4a1e17f577e577c898
MeterMatters <Geoffroydubruel@hotmail.fr>
2017-06-15 11:34:52 +0200

    Page de contact DOMISEP
    Utilisation d'API pour Google Maps, et des codes html de contact.

---
+++ Site/Site/vue/assistance.php
@@ -0,0 +1,20 @@
+
+
+<div id="contenu">
+  <form class="basic-grey">
+  <h2> Contacter DOMISEP </h2>
+  <h3>
+    <a href="mailto:cosyhouse6@gmail.com">Assistance Mail</a>
+  </h3>
+  <h3>
+    <a href="tel:0123456789">01 23 45 67 89</a>
+  </h3>
+    <h3>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1962.1410935828412!2d2.
+      278121793499162!3d48.8254125345965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.
+      1!3m3!1m2!1s0x0%3A0xe0d3eb2ad501cb27!2sISEP!5e0!3m2!1sfr!2sfr!4v1483356587046"
+      width="550" height="500" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </h3>
+  </form>
+</div>
+
