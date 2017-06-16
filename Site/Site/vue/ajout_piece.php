3b3145355e3c711424be87c912f6fd985ce3ee1c
bsoufiani <basile.soufiani@isep.fr>
2017-06-16 15:06:15 +0200

    Add files via upload

---
+++ Site/Site/vue/ajout_piece.php
@@ -0,0 +1,21 @@
+<body id="bodyautre">
+  <div id="contenu">
+
+<input value="Retour" class="boutonretour" require type="submit">
+<form method="POST" class="inscription" action="">
+
+<h3>Ajout d'une pièce</h3>
+
+
+<input id="piecem" type="text" name="piece" placeholder="Nom de la pièce">
+
+
+<input id="submitbutton" type="submit" name="AJOUTER" value="Ajouter"/>
+
+<p id="erreurchamp">
+  Maximum 23 caractères, espaces inclus.
+</p>
+</form>
+</div>
+
+</body>
