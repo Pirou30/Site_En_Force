589765aa0636e2b35fad8c94b33a3e9faed6d95b
MeterMatters <Geoffroydubruel@hotmail.fr>
2017-06-18 14:53:29 +0200

    Page d'accueil Administrateur

---
+++ Site/Site/vue/conditions_admin.php
@@ -0,0 +1,5 @@
+<div id="contenu">
+	<div id="Texte">
+	<a>Bienvenue à vous Administrateur ! Vous pouvez désormais visualiser la liste des utilisateurs, ainsi que gérer les clés de la base de donnée. </a>
+	</div>
+</div>
