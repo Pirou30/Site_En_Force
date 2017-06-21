1ca50570dd3f1be7c4514fa8c13f84ebb8abefbd
chrismignon1996 <chris.mignon@icloud.com>
2017-06-21 15:25:47 +0200

    Create objet.php

---
+++ Site/Site/vue/objet.php
@@ -0,0 +1,28 @@
+ <div class="menu-vertical">
+      <div class="mv-item">
+        <a href="index.php?page=ajout_objet">
+
+        <p> Ajouter un capteur </p>
+      </a>
+      </div>
+      <div class="mv-item">
+        <a href="index.php?page= <! on doit créer la page modifier objet >">    
+
+        <p> Modifier un capteur </p>
+        </a>
+      </div>
+
+
+      <div class="mv-item">
+        <a href="index.php?page= <!on doit créer une page supprimer objet>"> 
+
+        <p> Supprimer un capteur </p>
+      </a>
+      </div>
+      <div class="mv-item">
+        <a href="index.php?page= <! on va surement créer une page avec l'état de tous les objets> ">
+
+        <p> Etat des capteurs </p>
+        </a>
+      </div>
+  </div>
